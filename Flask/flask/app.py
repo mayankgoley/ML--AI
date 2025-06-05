84db5e01545d67c33a5d6079b724b9889d718fc0
Mayank Goley <42241477+mayankgoley@users.noreply.github.com>
2025-06-04 21:00:49 -0700

    Basics of Flask

---
+++ Flask/flask/app.py
@@ -0,0 +1,28 @@
+from flask import Flask
+
+''' It creates an instance of Flask class.
+    This instance is the WSGI(Web server gateway Interface) application.'''
+
+# This is a WSGI application instance.
+# Create an instance of the Flask class.
+app = Flask(__name__)
+
+@app.route('/')
+def welcome():
+    '''This function is a view function that handles requests to the root URL ('/').
+       It returns a simple welcome message.'''
+    
+    return "Welcome to the Flask application!. This is a simple Flask app."
+
+@app.route('/index')
+def index():
+    return "This is the index page of the Flask application."
+
+if __name__ == '__main__':
+    # If this script is run directly, start the Flask development server.
+    app.run(debug=True)
+
+
+'''Note: The debug=True option enables the debugger and reloader.
+This means that if you make changes to your code, the server will automatically reload.
+This is useful during development, but should be turned off in production. '''
