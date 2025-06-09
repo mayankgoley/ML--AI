065aec7e7cf7b0c18c50ff5a9c3f6d047563506d
Mayank Goley <42241477+mayankgoley@users.noreply.github.com>
2025-06-08 20:28:27 -0500

    HTTP verbs

--- Flask/flask/api.py
+++ Flask/flask/api.py
@@ -23,14 +23,43 @@ def get_items():
 
 ## Retrieve specific item by id
 
-@app.route('/items/<int: item_id>', methods = ['GET'])
+@app.route('/items/<int:item_id>', methods = ['GET'])
 def get_item(item_id):
     item = next((item for item in items if item["id"] == item_id), None)
     if item is None:
         return jsonify("error: item not found")
     return item
 
+@app.route('/items', methods=['POST'])
+def create_item():
+    if not request.json or not 'name' in request.json:
+        return jsonify({'Error: Item not found'})
+    new_item = {
+        "id": items[-1]['id'] + 1 if items else 1,
+        "name": request.json['name'],
+        "description": request.json["description"]
+    }
+    items.append(new_item)
+    return jsonify(new_item)
 
+## PUT: Update an existing item
+
+@app.route('/items/<int:item_id>', methods = ['PUT'])
+def update_item(item_id):
+    item = next((item for item in items if item["id"] == item_id), None)
+
+    if item is None:
+        return jsonify("Error: Item not found")
+    
+    item['name'] = request.json.get('name', item['name'])
+    item['description'] = request.json.get('description', item['description'])
+
+## DELETE - Delete an item
+@app.route('/items/<int:item_id>', methods = ['DELETE'])
+def delete_items(item_id):
+    global items
+    items = [item for item in items if item["id"] != item_id]
+    return jsonify({"result": "Item deleted"})
 
 if __name__ =='__main__':
     app.run(debug=True)
